26278869d7e16c93252aa57bec4c9665f5b1c44a
Tuan Dang Q <tuan.dang@asiantech.com.vn>
2018-12-26 09:56:36 +0700

    Re-Create project.

--- README.md
+++
@@ -1,6 +0,0 @@
-#Installing virtualenv-1.9 or greater
-$ [sudo] pip install virtualenv
-
-# Install Django and Django REST framework into the virtualenv
-pip install django
-pip install djangorestframework
--- auth_api/auth_api/urls.py
+++ auth_api/auth_api/urls.py
@@ -1,4 +1,4 @@
-"""api URL Configuration
+"""auth_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -18,5 +18,4 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('restapi.urls'))
 ]
