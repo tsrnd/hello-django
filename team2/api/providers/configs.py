74b4e6183975d9ae7be742b076782707aadb46fe
at-hungnguyen <hung.nguyen@asiantech.vn>
2019-01-02 17:52:58 +0700

    move redis into infra

--- team2/api/providers/configs.py
+++ team2/api/providers/configs.py
@@ -3,9 +3,10 @@ import inject
 from django.core.cache import cache
 from api.products.repositories import *
 from api.products.usecases import *
+from team2.infrastructure import Cache
 
 def api_providers_config(binder: inject.Binder):
     from django_redis import get_redis_connection
-    binder.bind(RepositoryInterface, ProductRepository(get_redis_connection("default")))
+    binder.bind(RepositoryInterface, ProductRepository(inject.attr(Cache)))
 
     binder.bind(UsecaseInterface, ProductUsecase())
