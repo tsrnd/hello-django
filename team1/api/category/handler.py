430bf6591b2180be46beb53d06f6883b382442f7
at-yenho <yen.ho@asiantech.vn>
2019-01-02 10:07:40 +0700

    add custom log

--- team1/api/category/handler.py
+++ team1/api/category/handler.py
@@ -5,11 +5,17 @@ from django.http import HttpResponse
 
 from api.category.usecase import Usecase
 from api.category.repository import Repository
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class Handler(View):
     view_factory = Usecase(repository=None)
 
     def get(self, request, *args, **kwargs):
-        cates = self.view_factory.get_all_category()
+        cates, err = self.view_factory.get_all_category()
+        if err is not None:
+            logger.error(err, "get all category error.")
+            return HttpResponse(json.dumps({"message": "Get categories error.", "error": err}), status=500, content_type='application/json')
         return HttpResponse(json.dumps(cates), status=200, content_type='application/json')
--- team1/api/category/repository.py
+++ team1/api/category/repository.py
@@ -6,13 +6,16 @@ class Repository:
         self.redis = redis
 
     def get_all_category(self):
-        cates_db = Category.objects.all()
         result = []
-        for cate in cates_db:
-            if self.redis.has_key("cat_"+str(cate.id)) == True:
-                self.redis.set("cat_"+str(cate.id), cate.name)
-            result.append({
-                'id': cate.id,
-                'name': cate.name,
-            })
-        return result
+        try:
+            cates_db = Category.objects.all()
+            for cate in cates_db:
+                if self.redis.has_key("cat_"+str(cate.id)) == True:
+                    self.redis.set("cat_"+str(cate.id), cate.name)
+                result.append({
+                    'id': cate.id,
+                    'name': cate.name,
+                })
+        except Exception as err:
+            return result, err
+        return result, None
