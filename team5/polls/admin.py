c9187215b212b8df3153531a924730e72d0ba671
Nghia Vo V <nghia.vo@asiantech.vn>
2019-01-03 09:45:16 +0700

    custom admin and model

--- team5/polls/admin.py
+++ team5/polls/admin.py
@@ -1,6 +1,20 @@
+""" docstring """
 from django.contrib import admin
 
 from .models import Choice, Question
+
+class ChoiceInLine(admin.StackedInline):
+    """ class docstring """
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    """ class docstring """
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
 # Register your models here.
 admin.site.register(Choice)
 admin.site.register(Question)
--- team5/polls/models.py
+++ team5/polls/models.py
@@ -1,16 +1,24 @@
+""" docstring """
 from django.db import models
 
 # Create your models here.
 
 class Question(models.Model):
+    """ func docstring """
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
- 
+
 class Choice(models.Model):
+    """ func docstring """
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+class Person(models.Model):
+    """ func docstring """
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
