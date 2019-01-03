a9cc7e51237b85bf649df9b7cd4c138ae9977225
Nghia Vo V <nghia.vo@asiantech.vn>
2019-01-03 09:10:47 +0700

    View and Template

--- team5/polls/views.py
+++ team5/polls/views.py
@@ -1,9 +1,35 @@
-from django.http import HttpResponse
+""" module docstring """
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from django.template import loader
 
+from .models import Question
 # Create your views here.
 
 def index(request):
-    response = HttpResponse()
-    response.write("<h1>Welcome</h1>")
-    response.write("This is the polls app")
-    return response
+    """ function docstring"""
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    """ function docstring"""
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question':question})
+
+
+def results(request, question_id):
+    """ function docstring"""
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    """ function docstring"""
+    return HttpResponse("You're voting on question %s." % question_id)
