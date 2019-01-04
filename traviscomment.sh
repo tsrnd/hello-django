5ea0f6a932a0e06d8503b7649fa20ad44214ea80
at-hungnguyen <hung.nguyen@asiantech.vn>
2019-01-04 10:46:33 +0700

    comment travis

--- traviscomment.sh
+++ traviscomment.sh
@@ -1,4 +1,4 @@
-# curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X POST \
-# -d "{\"body\": \"Hello world\"}" \
-# "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
-echo ${TRAVIS_JOB_WEB_URL}
+comment_body=$(curl https://api.travis-ci.com/v3/job/${TRAVIS_JOB_ID}/log.txt | sed -n 's/.*pylint-response://p')
+curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X POST \
+-d "{\"body\": \"$(comment_body)\"}" \
+"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
