afbd687edd98da9327a1220c9df482db2b70506f
ml-tam-nguyen4 <tam.nguyen4-fr@asiantech.vn>
2019-01-04 15:03:19 +0700

    implement test

--- polls/urls.py
+++ polls/urls.py
@@ -5,13 +5,11 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    # ex: /polls/
     path('', views.IndexView.as_view(), name='index'),
-    # ex: /polls/5/
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    # ex: /polls/5/results/
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('create/', views.QuestionCreate.as_view(), name='create'),
     path('<int:pk>/update/', views.QuestionUpdate.as_view(), name='update'),
+    path('<int:pk>/delete/', views.QuestionDelete.as_view(), name='delete'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,22 +1,10 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
-
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     context = {'latest_question_list': latest_question_list}
-#     return render(request, 'polls/index.html', context)
-
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
-
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question': question})
 
 
 class IndexView(generic.ListView):
@@ -32,6 +20,9 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultsView(generic.DetailView):
     model = Question
@@ -43,7 +34,6 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice.",
@@ -51,9 +41,6 @@ def vote(request, question_id):
     else:
         selected_choice.votes += 1
         selected_choice.save()
-        # Always return an HttpResponseRedirect after successfully dealing
-        # with POST data. This prevents data from being posted twice if a
-        # user hits the Back button.
         return HttpResponseRedirect(
             reverse('polls:results', args=(question.id, )))
 
@@ -62,3 +49,21 @@ class QuestionUpdate(generic.UpdateView):
     model = Question
     fields = ['question_text']
     template_name_suffix = '_update_form'
+
+
+class QuestionDelete(generic.DeleteView):
+    model = Question
+    success_url = reverse_lazy('polls:index')
+
+    def post(self, request, *args, **kwargs):
+        if "cancel" in request.POST:
+            self.object = self.get_object()
+            url = self.get_success_url()
+            return HttpResponseRedirect(url)
+        else:
+            return super(QuestionDelete, self).post(request, *args, **kwargs)
+
+
+class QuestionCreate(generic.CreateView):
+    model = Question
+    fields = ['question_text', 'pub_date']
