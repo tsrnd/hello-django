27448d8aa75d8eea1353f4207324f85b613655d3
at-hungnguyen <hung.nguyen@asiantech.vn>
2019-01-04 10:03:54 +0700

    testing

--- traviscomment.sh
+++ traviscomment.sh
@@ -1,3 +1,4 @@
-curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X POST \
--d "{\"body\": \"Hello world\"}" \
-"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+# curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X POST \
+# -d "{\"body\": \"Hello world\"}" \
+# "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+echo ${TRAVIS_JOB_WEB_URL}
