be19a6189b3cec285808a4c8af24ac10f1c13a35
at-nhan-nguyen2 <nhan.nguyen2@asiantech.vn>
2019-01-04 15:18:26 +0700

    basic app, view, admin, test

--- myproject/settings.py
+++ myproject/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'django.contrib.admin', 'django.contrib.auth',
     'django.contrib.contenttypes', 'django.contrib.sessions',
-    'django.contrib.messages', 'django.contrib.staticfiles', 'myapp'
+    'django.contrib.messages', 'django.contrib.staticfiles', 'myapp', 'polls'
 ]
 
 MIDDLEWARE = [
