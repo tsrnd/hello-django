bf4a25f1bb2839e6d7f84b417f5a7a66a87eff11
at-hungnguyen <hung.nguyen@asiantech.vn>
2019-01-04 13:45:12 +0700

    test coment

--- traviscomment.sh
+++ traviscomment.sh
@@ -1,4 +1,7 @@
 comment_body=$(curl https://api.travis-ci.com/v3/job/${TRAVIS_JOB_ID}/log.txt | sed -n 's/^pylint-response://p')
-curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X POST \
--d "{\"body\": \'$comment_body\'}" \
-"https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+echo $comment_body | \
+while read comment; do
+    curl -H "Authorization: token ${GITHUB_ACCESS_TOKEN}" -X POST \
+    -d "{\"body\": \'$comment\'}" \
+    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+done
