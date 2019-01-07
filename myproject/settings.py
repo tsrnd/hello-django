857206368b269e99990e8cbfc5382e61b2738316
phutran1 <justinphus@gmail.com>
2019-01-07 10:20:09 +0700

    follow tutorial

--- myproject/settings.py
+++ myproject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin', 'django.contrib.auth',
     'django.contrib.contenttypes', 'django.contrib.sessions',
     'django.contrib.messages', 'django.contrib.staticfiles', 'myapp'
