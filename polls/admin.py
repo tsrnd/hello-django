f4c570347b27763b166eeb734dd75d1080ea0866
Vu Dang N <vu.dang@asiantech.vn>
2019-01-07 16:12:10 +0700

    Customize view admin

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,27 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {
+        'fields': ['question_text']
+    }), ('Date information', {
+        'fields': ['pub_date']
+    })]
+
+    inlines = [ChoiceInline]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    list_filter = ['pub_date', 'question_text']
+
+    search_fields = ['question_text', 'pub_date']
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
