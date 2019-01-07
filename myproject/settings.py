6a02209bbdcc1178d6e52b40048932f436553f67
Tanh Le D <tanh.le@asiantech.vn>
2019-01-07 09:18:02 +0700

    Finishing tutorial 7

--- myproject/settings.py
+++ myproject/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -89,19 +89,23 @@ CACHES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation'
+        'NAME':
+        'django.contrib.auth.password_validation'
         '.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation'
+        'NAME':
+        'django.contrib.auth.password_validation'
         '.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation'
+        'NAME':
+        'django.contrib.auth.password_validation'
         '.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation'
+        'NAME':
+        'django.contrib.auth.password_validation'
         '.NumericPasswordValidator',
     },
 ]
