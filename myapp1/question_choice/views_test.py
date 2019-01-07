fbeddde48b242b7721d5d1ffcdaaea07425f6d3c
at-yenho <yen.ho@asiantech.vn>
2019-01-07 17:57:45 +0700

    unit test mock view

---
+++ myapp1/question_choice/views_test.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+import pytest
+import requests
+from django.http import HttpRequest, QueryDict
+from mock import patch
+from myapp1.question_choice.views import index
+from myapp1.question_choice.models import Question
+
+
+class Views_Test(TestCase):
+    @patch("myapp1.question_choice.models.Question.objects.order_by")
+    def test_index(self, mock_get_question):
+        mock_get_question.return_value = []
+        response = index(HttpRequest())
+        html = response.content.decode('utf8')
+        self.assertIn('<p>No polls are available.</p>', html)
