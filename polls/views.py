2fb728478b70904a9f28ecd6d2ab268286b22611
Vu Dang N <vu.dang@asiantech.vn>
2019-01-07 10:28:20 +0700

    Update view

--- polls/views.py
+++ polls/views.py
@@ -2,8 +2,27 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    template_name = 'polls/detail.html'
+    model = Question
+
+
+class ResulsView(generic.DeleteView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def index(request):
