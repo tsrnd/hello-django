71d8998ede8f7fc7ceb9c5097d4bf80ff1954c18
ha-phan <ha.phan@asiantech.vn>
2019-01-10 09:58:09 +0700

    form

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import Question, Choice
 
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the polls index.")
@@ -10,12 +10,12 @@ from django.http import Http404
 # def detail(request, question_id):
 #     return HttpResponse("You're looking at question %s." % question_id)
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+# def results(request, question_id):
+#     response = "You're looking at the results of question %s."
+#     return HttpResponse(response % question_id)
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+# def vote(request, question_id):
+#     return HttpResponse("You're voting on question %s." % question_id)
 
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -41,3 +41,21 @@ def detail(request, question_id):
     except Question.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
