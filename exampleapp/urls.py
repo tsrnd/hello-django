b4f7fc989bf8dea91275aa296cd616888d4e7cad
Nguyen Van Phuc <phuc.nguyen3@asiantech.vn>
2019-01-14 13:50:49 +0700

    fix email for ssh key

--- exampleapp/urls.py
+++ exampleapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'phucnguyen'
+app_name = 'phucnguyen';
 urlpatterns = [
     # path('time', views.current_datetime, name='Time current'),
     path('', views.index, name='index'),
--- myapp/foo/views.py
+++ myapp/foo/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    text = """Nothing here!"""
+    text = """Nothing here!""";
     return HttpResponse(text)
+
 
 def hello(request):
     text = """<h1>Hello Django!</h1>"""
